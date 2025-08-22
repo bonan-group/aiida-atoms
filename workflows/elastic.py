d4c9530eadac3976830dd927037ace54dd0f1aaa
fengxiang <fengxiang@LAPTOP-RIM7RET3.localdomain>
2025-08-22 15:01:41 +0800

    pre-commit fix

--- workflows/elastic.py
+++ workflows/elastic.py
@@ -1,21 +1,37 @@
-import numpy as np
-import spglib
-from ase.units import GPa
-from pymatgen.io.ase import AseAtomsAdaptor
-from pymatgen.analysis.elasticity import DeformedStructureSet, ElasticTensor, Strain, Stress
-from aiida.engine import calcfunction
-import ase
-from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
-from aiida import orm
-from aiida.engine import ToContext, WorkChain, append_, if_, while_
-from aiida.orm.nodes.data.base import to_aiida_type
-from aiida.plugins import WorkflowFactory
+"""
+Elastic workflow for AiiDA-VASP.
+
+This module implements a workflow to calculate elastic constants, including:
+    - Strain generation for crystal structures
+    - Stress calculation via VASP relaxation
+    - Elastic tensor composition and validation
+"""
+
+
 from aiida_vasp.utils.extended_dicts import update_nested_dict, update_nested_dict_node
 from aiida_vasp.utils.workchains import compose_exit_code
 from aiida_vasp.workchains.v2.relax import VaspRelaxWorkChain
+import ase
+from ase.units import GPa
+import numpy as np
+from pymatgen.analysis.elasticity import (
+    DeformedStructureSet,
+    ElasticTensor,
+    Strain,
+    Stress,
+)
+from pymatgen.io.ase import AseAtomsAdaptor
+from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
+import spglib
+
+from aiida import orm
+from aiida.engine import ToContext, WorkChain, append_, calcfunction, if_, while_
+from aiida.orm.nodes.data.base import to_aiida_type
+from aiida.plugins import WorkflowFactory
 
 from .common import OVERRIDE_NAMESPACE, site_magnetization_to_magmom
 from .mixins import WithBuilderUpdater
+
 
 class VaspElasticWorkChain(WorkChain, WithBuilderUpdater):
     """
@@ -30,15 +46,25 @@ class VaspElasticWorkChain(WorkChain, WithBuilderUpdater):
     @classmethod
     def define(cls, spec):
         super().define(spec)
-        spec.input('elastic_settings', valid_type=orm.Dict, 
-                   required=False,
-                   help='Settings for the elastic tensor calculation, valid options are: use_symmetry, symprec',
-                   )
-        #基于VaspRelaxWorkChain的设置输入
-        spec.expose_inputs(VaspRelaxWorkChain, 'relax') 
-        spec.output('elastic_tensor', valid_type=orm.ArrayData)
-        spec.output('relaxed_structure', valid_type=orm.StructureData, help='The fully relaxed structure after the full relaxation step')
-        spec.output('primitive_structure', valid_type=orm.StructureData, help='The standardized primitive structure or unit cell after the full relaxation step')
+        spec.input(
+            "elastic_settings",
+            valid_type=orm.Dict,
+            required=False,
+            help="Settings for the elastic tensor calculation, valid options are: use_symmetry, symprec",
+        )
+        # 基于VaspRelaxWorkChain的设置输入
+        spec.expose_inputs(VaspRelaxWorkChain, "relax")
+        spec.output("elastic_tensor", valid_type=orm.ArrayData)
+        spec.output(
+            "relaxed_structure",
+            valid_type=orm.StructureData,
+            help="The fully relaxed structure after the full relaxation step",
+        )
+        spec.output(
+            "primitive_structure",
+            valid_type=orm.StructureData,
+            help="The standardized primitive structure or unit cell after the full relaxation step",
+        )
 
         # Outline of the workchain,steps to be executed
         spec.outline(
@@ -50,31 +76,37 @@ class VaspElasticWorkChain(WorkChain, WithBuilderUpdater):
         )
         spec.exit_code(
             500,
-            'ERROR_SUB_PROCESS_FAILED',
-            message='The subprocess has failed.',
+            "ERROR_SUB_PROCESS_FAILED",
+            message="The subprocess has failed.",
         )
         spec.exit_code(
             501,
-            'ERROR_ELASTIC_TENSOR_COMPOSITION',
-            message='The elastic tensor could not be composed from the deformed structure relaxations.',
+            "ERROR_ELASTIC_TENSOR_COMPOSITION",
+            message="The elastic tensor could not be composed from the deformed structure relaxations.",
         )
-        
+
     def setup(self):
         """
         Initialize context variables
         """
-        
-        self.ctx.relax_inputs = self.exposed_inputs(VaspRelaxWorkChain, 'relax')
-        
+
+        self.ctx.relax_inputs = self.exposed_inputs(VaspRelaxWorkChain, "relax")
+
         pdict = self.ctx.relax_inputs.vasp.parameters.get_dict()
-        self.ctx.elastic_settings = {} if 'elastic_settings' not in self.inputs else self.inputs.elastic_settings
-        pdict['incar'].pop('ibrion', None)
-        pdict['incar'].pop('isif', None)
-        pdict['incar'].pop('nsw', None)
+        self.ctx.elastic_settings = (
+            {}
+            if "elastic_settings" not in self.inputs
+            else self.inputs.elastic_settings
+        )
+        pdict["incar"].pop("ibrion", None)
+        pdict["incar"].pop("isif", None)
+        pdict["incar"].pop("nsw", None)
         if pdict != self.ctx.relax_inputs.vasp.parameters.get_dict():
-            self.logger.warn('The VASP parameters have been modified to remove `ibrion`, `isif`, and `nsw` settings for the elastic tensor calculation.')
+            self.logger.warn(
+                "The VASP parameters have been modified to remove `ibrion`, `isif`, and `nsw` settings for the elastic tensor calculation."
+            )
             self.ctx.relax_inputs.vasp.parameters = orm.Dict(dict=pdict)
-            
+
         self.ctx.deformed_structures = []
         self.ctx.deformed_strains = []
         self.ctx.workchains = {}
@@ -83,10 +115,10 @@ class VaspElasticWorkChain(WorkChain, WithBuilderUpdater):
         """
         Run a full relaxation of the input structure
         """
-        self.report('Running full relaxation')
+        self.report("Running full relaxation")
         relax_inputs = self.ctx.relax_inputs
-        #relax_inputs.structure = self.inputs.structure
-        #self.base_workchain的计算参数和输入结构由外部导入
+        # relax_inputs.structure = self.inputs.structure
+        # self.base_workchain的计算参数和输入结构由外部导入
         running = self.submit(self._base_workchain, **relax_inputs)
         return ToContext(full_relax=running)
 
@@ -95,75 +127,110 @@ class VaspElasticWorkChain(WorkChain, WithBuilderUpdater):
         Standardize the structure after the full relaxation
         You can use spglib to standardize the structure or pymatgen's SpacegroupAnalyzer.
         """
-        self.report('Standardizing the structure')
+        self.report("Standardizing the structure")
         relaxed_structure = self.ctx.full_relax.outputs.relax.structure
-        self.out('relaxed_structure',relaxed_structure)
-        primitive_type = self.ctx.elastic_settings.get('primitive_type','conventional')
-        if primitive_type == 'conventional':
+        self.out("relaxed_structure", relaxed_structure)
+        primitive_type = self.ctx.elastic_settings.get("primitive_type", "conventional")
+        if primitive_type == "conventional":
             # Use pymatgen's SpacegroupAnalyzer to get the conventional standard structure
-            self.report('Using pymatgen SpacegroupAnalyzer to standardize the structure')
-            conventional_structure = SpacegroupAnalyzer(relaxed_structure.get_pymatgen()).get_conventional_standard_structure()
-            self.ctx.reference_structure = orm.StructureData(pymatgen=conventional_structure)
-        elif primitive_type == 'primitive':
+            self.report(
+                "Using pymatgen SpacegroupAnalyzer to standardize the structure"
+            )
+            conventional_structure = SpacegroupAnalyzer(
+                relaxed_structure.get_pymatgen()
+            ).get_conventional_standard_structure()
+            self.ctx.reference_structure = orm.StructureData(
+                pymatgen=conventional_structure
+            )
+        elif primitive_type == "primitive":
             atoms = relaxed_structure.get_ase()
             # standardize the structure using spglib
             # Use spglib to standardize the structure
-            prim_atoms_data = spglib.find_primitive((atoms.cell, atoms.get_scaled_positions(), atoms.numbers), 
-                                                    symprec=self.ctx.elastic_settings.get('symprec', 1e-3))
+            prim_atoms_data = spglib.find_primitive(
+                (atoms.cell, atoms.get_scaled_positions(), atoms.numbers),
+                symprec=self.ctx.elastic_settings.get("symprec", 1e-3),
+            )
             primitive_lat, primitive_pos, primitive_numbers = prim_atoms_data
-            primitive_atoms = ase.Atoms(cell=primitive_lat, scaled_positions=primitive_pos, numbers=primitive_numbers, pbc=True)
+            primitive_atoms = ase.Atoms(
+                cell=primitive_lat,
+                scaled_positions=primitive_pos,
+                numbers=primitive_numbers,
+                pbc=True,
+            )
             self.ctx.reference_structure = orm.StructureData(ase=primitive_atoms)
         else:
-            raise ValueError(f'Unknown primitive type: {primitive_type}. Supported types are "conventional" and "primitive".')
-        self.out('primitive_structure', self.ctx.reference_structure)
-        
+            raise ValueError(
+                f'Unknown primitive type: {primitive_type}. Supported types are "conventional" and "primitive".'
+            )
+        self.out("primitive_structure", self.ctx.reference_structure)
+
     def run_relax_multi(self):
         """
         Run multiple relaxation calculations for the deformed structures
         """
-        deformed = generate_deformed_structures(self.ctx.reference_structure,
-                                                normal_strains=orm.List(self.ctx.elastic_settings.get('normal_strains', None)),
-                                                shear_strains=orm.List(self.ctx.elastic_settings.get('shear_strains', None)),
-                                                symmetry=self.ctx.elastic_settings.get('use_symmetry', True)
+        deformed = generate_deformed_structures(
+            self.ctx.reference_structure,
+            normal_strains=orm.List(
+                self.ctx.elastic_settings.get("normal_strains", None)
+            ),
+            shear_strains=orm.List(
+                self.ctx.elastic_settings.get("shear_strains", None)
+            ),
+            symmetry=self.ctx.elastic_settings.get("use_symmetry", True),
         )
         launched_calculations = {}
         inputs = self.ctx.relax_inputs
         base_relax_settings = inputs.relax_settings.get_dict()
         # Update the base relaxation settings for the deformed structures
-        base_relax_settings['volume'] = False  
-        base_relax_settings['shape'] = False
+        base_relax_settings["volume"] = False
+        base_relax_settings["shape"] = False
 
         for key, value in deformed.items():
-            if key.startswith('structure_'):
+            if key.startswith("structure_"):
                 d_structure = value
                 inputs.structure = d_structure
                 relax_settings = base_relax_settings.copy()
-                relax_settings['label'] = f'relax_deformed_{key}'
+                relax_settings["label"] = f"relax_deformed_{key}"
                 inputs.relax_settings = orm.Dict(dict=relax_settings)
                 running = self.submit(self._base_workchain, **inputs)
-                launched_calculations[f'workchain_deformed_'+key] = running
-            if key.startswith('deformation_strains'):
-                self.ctx.deformations = value  #为orm.array数据类型
+                launched_calculations[f"workchain_deformed_" + key] = running
+            if key.startswith("deformation_strains"):
+                self.ctx.deformations = value  # 为orm.array数据类型
         return ToContext(**launched_calculations)
-        
+
     def compose_elastic_tensor(self):
         """
         Get the elastic tensor from by compose_elastic_tensor method
         """
-        self.report('Composing the elastic tensor from the deformed structure relaxations')
-        miscs = {key: self.ctx[key].outputs.misc for key in self.ctx if key.startswith('workchain_deformed')}
-        self.report('finished collecting workchain outputs for elastic tensor composition')
+        self.report(
+            "Composing the elastic tensor from the deformed structure relaxations"
+        )
+        miscs = {
+            key: self.ctx[key].outputs.misc
+            for key in self.ctx
+            if key.startswith("workchain_deformed")
+        }
+        self.report(
+            "finished collecting workchain outputs for elastic tensor composition"
+        )
         # Check  the deformations data and miscs data
-        self.report(f'deform_datas: {self.ctx.deformations.get_array("deformation_strains")}')
-        self.report(f'miscs: {miscs}')
-        self.out('elastic_tensor', get_elastic_tensor(deform_datas=self.ctx.deformations, **miscs))
-    
+        self.report(
+            f'deform_datas: {self.ctx.deformations.get_array("deformation_strains")}'
+        )
+        self.report(f"miscs: {miscs}")
+        self.out(
+            "elastic_tensor",
+            get_elastic_tensor(deform_datas=self.ctx.deformations, **miscs),
+        )
+
 
 @calcfunction
-def generate_deformed_structures(structure: orm.StructureData,
-                                 normal_strains: orm.List,
-                                 shear_strains: orm.List,
-                                 symmetry: bool = True):    
+def generate_deformed_structures(
+    structure: orm.StructureData,
+    normal_strains: orm.List,
+    shear_strains: orm.List,
+    symmetry: bool = True,
+):
     if normal_strains is None:
         normal_strains = (-0.01, -0.005, 0.005, 0.01)
     else:
@@ -172,22 +239,28 @@ def generate_deformed_structures(structure: orm.StructureData,
         shear_strains = (-0.06, -0.03, 0.03, 0.06)
     else:
         shear_strains = shear_strains.get_list()
-    deformed_structures = DeformedStructureSet(structure.get_pymatgen(),
-                                                norm_strains=normal_strains,
-                                                shear_strains=shear_strains,
-                                                symmetry=bool(symmetry)
-                                                )
+    deformed_structures = DeformedStructureSet(
+        structure.get_pymatgen(),
+        norm_strains=normal_strains,
+        shear_strains=shear_strains,
+        symmetry=bool(symmetry),
+    )
     output = {}
     for i, d_structure in enumerate(deformed_structures.deformed_structures):
         # 转换为 AiiDA StructureData
         structure_data = orm.StructureData(pymatgen=d_structure)
         structure_data.label = f"deformed_{i}"
-        structure_data.description = f"Deformed structure {i} for elastic tensor calculation"
-        output[f'structure_{i}'] = structure_data
+        structure_data.description = (
+            f"Deformed structure {i} for elastic tensor calculation"
+        )
+        output[f"structure_{i}"] = structure_data
     strain_data = orm.ArrayData()
-    strain_data.set_array('deformation_strains', np.array(deformed_structures.deformations))
-    output['deformation_strains'] = strain_data
+    strain_data.set_array(
+        "deformation_strains", np.array(deformed_structures.deformations)
+    )
+    output["deformation_strains"] = strain_data
     return output
+
 
 def get_elastic_tensor(deform_datas: orm.ArrayData, **kwargs):
     """
@@ -195,31 +268,33 @@ def get_elastic_tensor(deform_datas: orm.ArrayData, **kwargs):
     """
     strains = []
     stresses = []
-    #得到弛豫完后结构的应力，再得到对应的应变，然后
-    #elastic_tensor = ElasticTensor.from_diff_fit(strains, stresses)得到弹性张量
-    deform_datas = deform_datas.get_array('deformation_strains')
-    
+    # 得到弛豫完后结构的应力，再得到对应的应变，然后
+    # elastic_tensor = ElasticTensor.from_diff_fit(strains, stresses)得到弹性张量
+    deform_datas = deform_datas.get_array("deformation_strains")
+
     miscs = []
-    for key,value in kwargs.items():
-        if key.startswith('workchain_deformed'):
-            idx = int(key.split('_')[-1])
-            miscs.append([idx,value])
-    
+    for key, value in kwargs.items():
+        if key.startswith("workchain_deformed"):
+            idx = int(key.split("_")[-1])
+            miscs.append([idx, value])
+
     miscs = sorted(miscs, key=lambda x: x[0])
 
-    for idx,misc in miscs:
-        stress = -np.array(misc.get_dict()['stress']) * 0.1 # Convert units to GPa from kBar
+    for idx, misc in miscs:
+        stress = (
+            -np.array(misc.get_dict()["stress"]) * 0.1
+        )  # Convert units to GPa from kBar
         stress_matrix = Stress(stress)
         stresses.append(stress_matrix)
-        
+
         strain_matrix = Strain.from_deformation(deform_datas[idx])
-        strains.append(strain_matrix)    
+        strains.append(strain_matrix)
     try:
         elastic_tensor = ElasticTensor.from_diff_fit(strains, stresses)
     except Exception as e:
-        raise ValueError(f'Failed to compose the elastic tensor: {e}')
-    
+        raise ValueError(f"Failed to compose the elastic tensor: {e}")
+
     elastic_array = orm.ArrayData()
-    elastic_array.set_array('elastic_tensor', elastic_tensor.voigt)
+    elastic_array.set_array("elastic_tensor", elastic_tensor.voigt)
     elastic_array.store()
     return elastic_array
